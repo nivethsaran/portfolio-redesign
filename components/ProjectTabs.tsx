d0ae98e96825baaa3659c4b0ebaeb23784fbb25c
Niveth Saran <31360464+nivethsaran@users.noreply.github.com>
2023-10-18 14:53:05 +0530

    fix compile issue

--- components/ProjectTabs.tsx
+++ components/ProjectTabs.tsx
@@ -18,7 +18,7 @@ const gradients = {
 }
 
 import Container from './Container'
-import projectsData from '@/data/projectsData.1'
+import projectsData from '@/data/projectsData'
 const features = [
   {
     title: 'Payroll',
